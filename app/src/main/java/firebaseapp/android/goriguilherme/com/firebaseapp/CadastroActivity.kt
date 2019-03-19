c19be929f2fa2d932df73109192618725716d94a
Guilherme Cesar Gori <goriguilherme@MacBook-Pro-de-Guilherme.local>
2019-03-19 20:58:33 -0300

    cadastrar novo usuario

--- app/src/main/java/firebaseapp/android/goriguilherme/com/firebaseapp/CadastroActivity.kt
+++ app/src/main/java/firebaseapp/android/goriguilherme/com/firebaseapp/CadastroActivity.kt
@@ -2,11 +2,52 @@ package firebaseapp.android.goriguilherme.com.firebaseapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.content.Intent
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_cadastro.*
+
 
 class CadastroActivity : AppCompatActivity() {
+
+    val timer = object: CountDownTimer(500,1000) {
+        override fun onTick(millisUntilFinished: Long) {}
+        override fun onFinish() {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+        btnConfirmarCadastro.setOnClickListener {
+            cadastrarUsuario(txtEmail.text.toString(), txtPassword.text.toString())
+        }
+    }
+
+    private fun cadastrarUsuario(email: String, password: String) {
+        val mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(applicationContext, "Cadastro efetuado com sucesso!",
+                        Toast.LENGTH_SHORT).show()
+                    timer.start()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        applicationContext, "Não foi possível cadastrar o usuário.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
--- app/src/main/java/firebaseapp/android/goriguilherme/com/firebaseapp/ResetarSenhaActivity.kt
+++ app/src/main/java/firebaseapp/android/goriguilherme/com/firebaseapp/ResetarSenhaActivity.kt
@@ -1,12 +1,64 @@
 package firebaseapp.android.goriguilherme.com.firebaseapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.provider.ContactsContract
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_resetar_senha.*
 
 class ResetarSenhaActivity : AppCompatActivity() {
+
+    val timer = object: CountDownTimer(500,1000) {
+        override fun onTick(millisUntilFinished: Long) {}
+        override fun onFinish() {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resetar_senha)
+
+        btnResetarSenha.setOnClickListener {
+            enviarEmailRecuperacaoSenha(txtEmail.text.toString())
+        }
     }
+
+
+    private fun enviarEmailRecuperacaoSenha(email: String) {
+        val auth = FirebaseAuth.getInstance()
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                task ->  if (task.isSuccessful) {
+            Toast.makeText(applicationContext, "E-mail enviado com sucesso!",
+                Toast.LENGTH_SHORT).show()
+            timer.start()
+        } else {
+            Toast.makeText(applicationContext, "Não foi possível resetar a senha",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        }
+    }
+
 }
+
+
+
+//private fun sendPasswordReset() {
+//    // [START send_password_reset]
+//    val auth = FirebaseAuth.getInstance()
+//    val emailAddress = "user@example.com"
+//
+//    auth.sendPasswordResetEmail(emailAddress)
+//        .addOnCompleteListener { task ->
+//            if (task.isSuccessful) {
+//                Log.d(TAG, "Email sent.")
+//            }
+//        }
+//    // [END send_password_reset]
+//}
